f5740cd35e5cff4cd26195b42680bbdd6f73b6f6
EvanSevin <evan.sevin.cours@gmail.com>
2022-03-26 16:55:28 +0000

    feat: webSocket init

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,15 +1,38 @@
-use actix_web::{App, HttpServer};
+//! Simple echo websocket server.
+//!
+//! Open `http://localhost:8080/` in browser to test.
 
-mod routes;
+use actix_files::NamedFile;
+use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web_actors::ws;
+
+mod server;
+use self::server::MyWebSocket;
+
+async fn index() -> impl Responder {
+    NamedFile::open_async("./static/index.html").await.unwrap()
+}
+
+/// WebSocket handshake and start `MyWebSocket` actor.
+async fn echo_ws(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    ws::start(MyWebSocket::new(), &req, stream)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    log::info!("starting HTTP server at http://localhost:3000");
+
     HttpServer::new(|| {
         App::new()
-            .service(routes::test::test)
-            .service(routes::test::echo)
+            // websocket route
+            .service(web::resource("/ws").route(web::get().to(echo_ws)))
+            // enable logger
+            .wrap(middleware::Logger::default())
     })
-    .bind("0.0.0.0:3000")?
+    .workers(2)
+    .bind(("127.0.0.1", 3000))?
     .run()
     .await
 }
